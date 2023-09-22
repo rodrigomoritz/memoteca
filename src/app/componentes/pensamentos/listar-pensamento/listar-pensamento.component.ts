644d0ef0d9e14ca82442e9fa22ce5563cfad7776
Rodrigo Moritz <rodrigomoritz.br@gmail.com>
2023-09-21 21:58:11 -0300

    metodo criar pensamento

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -10,9 +11,14 @@ export class ListarPensamentoComponent implements OnInit {
 
   listarPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+
+    this.service.listar().subscribe( (listarPensamentos) => {
+      this.listarPensamentos = listarPensamentos;
+    });
+
   }
 
 }
